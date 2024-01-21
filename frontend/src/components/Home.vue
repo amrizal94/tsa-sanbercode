e3e25db28412d8bd0c23013f8d9c879ee0ad65fd
Ari Muhammad Rizal <arimrizal94@gmail.com>
2024-01-21 22:53:26 +0700

    login with state management vuex

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,16 +1,75 @@
 <template lang="">
   <div>
-    <h3>HOME</h3>
+    <div role="alert" class="alert alert-error text-lg" v-if="!books">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="stroke-current shrink-0 h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+      <span>Please login first.</span>
+      <div>
+        <router-link to="/login">
+          <button class="btn btn-sm btn-primary">OK</button>
+        </router-link>
+      </div>
+    </div>
+    <div class="overflow-x-auto" v-if="books">
+      <table class="table">
+        <!-- head -->
+        <thead class="text-lg bg-slate-200">
+          <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Image</th>
+            <th>Release Year</th>
+            <th>Thickness</th>
+            <th>Price</th>
+            <th>Total Page</th>
+            <th>Category</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <!-- row 1 -->
+          <tr v-for="book in books" :key="book.id">
+            <td>{{ book.title }}</td>
+            <td>{{ book.description }}</td>
+            <td>
+              <div class="avatar">
+                <div class="w-24 rounded">
+                  <img v-bind:src="book.image_url" />
+                </div>
+              </div>
+            </td>
+            <td>{{ book.release_year }}</td>
+            <td>{{ book.thickness }}</td>
+            <td>{{ book.price }}</td>
+            <td>{{ book.total_page }}</td>
+            <td>{{ book.category.name }}</td>
+            <td class="flex gap-5 items-center">
+              <v-icon name="co-trash" scale="1.5" />
+              <v-icon name="px-edit-box" scale="1.5" />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 <script>
-import axios from "axios";
+import { mapGetters } from "vuex";
 export default {
   name: "Home",
-  async created() {
-    const response = await axios.get("books");
-    console.log(response);
-    console.log(localStorage.getItem("token"));
+  computed: {
+    ...mapGetters(["books"]),
   },
 };
 </script>
--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -1,10 +1,18 @@
 <template>
   <nav class="navbar bg-neutral text-neutral-content">
     <div class="flex-1">
-      <a class="btn btn-ghost text-xl">{{ msg }}</a>
+      <a class="btn btn-ghost text-xl">Library</a>
     </div>
     <div class="flex-none">
-      <ul class="menu menu-horizontal px-1">
+      <ul class="menu menu-horizontal px-1" v-if="books">
+        <li
+          class="bg-slate-700 mx-2 hover:bg-gray-500"
+          @click="handleClickSignOut"
+        >
+          Sign out
+        </li>
+      </ul>
+      <ul class="menu menu-horizontal px-1" v-if="!books">
         <li class="bg-slate-700 mx-2 hover:bg-gray-500">
           <router-link to="/register">Sign Up</router-link>
         </li>
@@ -17,10 +25,34 @@
 </template>
 
 <script>
+import Swal from "sweetalert2";
+import { mapGetters } from "vuex";
 export default {
   name: "Nav",
-  props: {
-    msg: String,
+  computed: {
+    ...mapGetters(["books"]),
+  },
+  data() {
+    return {
+      token: null,
+    };
+  },
+  created() {
+    this.token = localStorage.getItem("token");
+  },
+  methods: {
+    handleClickSignOut() {
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Sign out success",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      localStorage.removeItem("token");
+      this.$store.dispatch("books", null);
+      this.$router.push("/login");
+    },
   },
 };
 </script>
