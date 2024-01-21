488edeb4f27276e5107db61360012db581154b06
Ari Muhammad Rizal <arimrizal94@gmail.com>
2024-01-21 15:56:53 +0700

    slicing and routing login, register, home

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,34 @@
+<template lang="">
+  <div class="bg-slate-500 w-1/2 p-5 rounded-2xl">
+    <h1 class="text-yellow-50 font-serif text-4xl tracking-wider text-center">
+      Sign In
+    </h1>
+    <form
+      action=""
+      method="post"
+      class="p-5 box-border flex flex-col justify-center gap-2"
+    >
+      <input
+        type="text"
+        placeholder="Username"
+        class="input input-bordered w-full"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        class="input input-bordered w-full"
+      />
+      <button class="btn w-full">Sign In</button>
+      <div class="label">
+        <span class="label-text-alt">Forget Password</span>
+        <span class="label-text-alt">Sign Up</span>
+      </div>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  name: "Login",
+};
+</script>
+<style lang=""></style>
---
+++ frontend/src/components/Nav.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar bg-neutral text-neutral-content">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl">{{ msg }}</a>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li class="bg-slate-700 mx-2 hover:bg-gray-500"><a>Sign Up</a></li>
+        <li class="bg-slate-700 mx-2 hover:bg-gray-500"><a>Sign In</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Nav",
+  props: {
+    msg: String,
+  },
+};
+</script>
