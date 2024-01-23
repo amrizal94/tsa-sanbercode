add63800dd4219de4b46cc108e87f10dbec9f41c
Ari Muhammad Rizal <arimrizal94@gmail.com>
2024-01-23 17:01:47 +0700

    ft:add category

---
+++ frontend/src/components/AddCategory.vue
@@ -0,0 +1,54 @@
+<template lang="">
+  <form class="flex flex-col gap-5" @submit.prevent="handleSubmit">
+    <h3 class="font-bold text-lg">Add New Category</h3>
+    <div class="flex gap-5">
+      <input
+        type="text"
+        placeholder="Name of the new category"
+        class="input input-bordered w-full max-w-xs"
+        v-model="name"
+        required
+      />
+      <button class="btn" type="submit">Button</button>
+    </div>
+  </form>
+</template>
+<script>
+import axios from "axios";
+import Swal from "sweetalert2";
+export default {
+  name: "AddCategory",
+  data() {
+    return {
+      name: null,
+    };
+  },
+  methods: {
+    async handleSubmit() {
+      try {
+        const response = await axios.post("categories", {
+          name: this.name,
+        });
+        Swal.fire({
+          position: "top-right",
+          icon: "success",
+          title: "Add Category",
+          text: response.data.message,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        this.$store.dispatch("isModalOpen", false);
+      } catch (error) {
+        Swal.fire({
+          position: "center",
+          icon: "error",
+          title: "Add Category",
+          text: error.response.data.message,
+          confirmButtonText: "Oke",
+        });
+      }
+    },
+  },
+};
+</script>
+<style lang=""></style>
